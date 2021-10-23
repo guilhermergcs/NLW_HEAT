7f8afef485e140af4d2ca9cdbce4f74f53f8b464
Guilherme Ribeiro <81856749+guilhermergcs@users.noreply.github.com>
2021-10-23 15:14:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ changeSocialMediaLinks()
 function getGitHubProfileInfos() {
   const url = `https://api.github.com/users/${linksSocialMedia.github}`
 
-  fecth(url)
+  fetch(url)
     .then(response => response.json())
     .then(data => {
       userName.textContent = data.name
